a13051291c67c04ba4cd50faf7eadee6aa8f010a
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-09-25 21:41:09 +0900

    条件分岐

--- php1/chap2/check_bmi.php
+++ php1/chap2/check_bmi.php
@@ -1,9 +1,4 @@
 <?php
-function sayHello($name){
-	$aisatu = "こんにちは！{$name}さん";
-	return $aisatu;
-}
-
 function bmi($height, $mass){ //身長と体重
 	$height = $height / 100;
 	$mass = $mass / ($height * $height);
@@ -17,9 +12,42 @@ function bmi($height, $mass){ //身長と体重
 	<title>関数の動作確認</title>
 </head>
 <body>
-<p><?php echo sayHello("やまうちきみゆき"); ?></p>
-<p><?php echo sayHello("山田太郎"); ?>
-あなたのBMI値は<?php echo bmi(170, 50); ?>です。</p>
-	
+<?php
+
+//var_dump($_POST);
+
+if(isset($_POST['_submit_check'])){
+
+//$bmi = bmi(170, 50);
+//$bmi = bmi(166, 80);
+//$bmi = bmi(166, 60);
+	$bmi = bmi($_POST['height'], $_POST['mass']);
+
+$bmi = round($bmi, 1);
+
+print "BMI値は{$bmi}で、";
+?>
+
+<?php if($bmi < 18.5): ?>
+やせ過ぎです
+<?php endif; ?>
+<?php if ($bmi > 25) : ?>
+太り過ぎです
+<?php else : ?>
+標準です
+<?php endif; ?>
+
+<?php }else{ ?>
+
+	<form method="POST" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
+身長：
+  <input type="text" name="height">
+<br />体重：
+  <input type="text" name="mass">
+<br />
+  <input type="submit" value="BMI値計算">
+  <input type="hidden" name="_submit_check" value="1">
+</form>
+<?php } ?>
 </body>
 </html>
