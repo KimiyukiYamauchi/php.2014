6e69a3a255c53b4d0d2f1df496e3e9a314ab0fcf
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-10-02 21:05:55 +0900

    リファクタリング、関数化

---
+++ php1/chap3/uranai.php
@@ -0,0 +1,41 @@
+<?php
+function uranai($uranai){
+	// 範囲を求めて$min/$max変数に記憶
+	$min = 0;
+	$max = count($uranai) - 1;
+
+	// mt_rand()関数の結果を$key変数に記憶
+	$key = mt_rand($min, $max);
+
+	// 結果を$result変数に記憶
+	$result = $uranai[$key];
+
+	return $result;
+}
+
+// 結果配列を用意する
+$uranai[] = "大吉ですおめでとうございます";
+$uranai[] = "大吉臨時収入があるかもしれません";
+$uranai[] = "大吉です今日は楽しく過ごせるでしょう";
+$uranai[] = "中吉です街に出かけるといいことがあるでしょう";
+$uranai[] = "小吉今日はまったり過ごしてみては";
+$uranai[] = "末吉ですPHPの勉強をするといいことがあるでしょう";
+$uranai[] = "大凶です今日は自宅でゆっくり過ごしてください";
+
+var_dump($uranai);
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>おみくじ</title>
+</head>
+<body>
+
+<p>あなたが引いたおみくじの結果は、「<?php print uranai($uranai); ?>」</p>
+<!--<a href="<?php echo $_SERVER['SCRIPT_NAME']; ?>">もう一度</a>-->
+<button onclick="location.reload()">もう一度</button>
+
+</body>
+</html>
