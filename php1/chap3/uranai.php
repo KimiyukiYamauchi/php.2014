69ff3cd884b137ba5457c59e24753d0b83d3cbcb
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-10-02 21:43:42 +0900

    関数を外部ファイル化

--- php1/chap3/uranai.php
+++ php1/chap3/uranai.php
@@ -1,17 +1,6 @@
 <?php
-function select_random($array){
-	// 範囲を求めて$min/$max変数に記憶
-	$min = 0;
-	$max = count($array) - 1;
-
-	// mt_rand()関数の結果を$key変数に記憶
-	$key = mt_rand($min, $max);
-
-	// 結果を$result変数に記憶
-	$result = $array[$key];
-
-	return $result;
-}
+//include("common.php");
+require_once 'common.php';
 
 // 結果配列を用意する
 $uranai[] = "大吉ですおめでとうございます";
