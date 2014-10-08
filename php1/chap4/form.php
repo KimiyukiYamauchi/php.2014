15b6f0e8761a4ebe676318f89152678343935cb1
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-10-08 19:32:23 +0900

    選択肢の中から１つ選択するための部品

--- php1/chap4/form.php
+++ php1/chap4/form.php
@@ -19,6 +19,17 @@
 備考<br />
 <textarea name="note" cols="40" rows="5">
 </textarea><br />
+性別<br />
+<input type="radio" name="sex" value="1">男性
+<input type="radio" name="sex" value="2">女性
+<input type="radio" name="sex" value="9">回答しない<br />
+都道府県<br />
+<select name="pref">
+<option value="1">北海道</option>
+<option value="2">青森県</option>
+<option value="3">岩手県</option>
+<option value="47">沖縄県</option>
+</select><br />
 
 <input type="submit" value="データ送信">
 </form>
