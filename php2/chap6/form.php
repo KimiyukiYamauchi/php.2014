fc3e5f3be61ba2951d06cb2ed5e7fbe55a2313a2
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-11-26 22:01:42 +0900

    form.php修正(テキストp108まで)

--- php2/chap6/form.php
+++ php2/chap6/form.php
@@ -1,16 +1,40 @@
 <?php
+$sweets = array('シュークリーム',
+								'ショートケーキ',
+								'モンブラン',
+								'チョコレートケーキ');
+
 // フォームがサブミットされたときに何かをする
 function process_form(){
 	print "Hello, ". $_POST['my_name'];
 }
 
 // フォームを表示
-function show_form(){
+function show_form($errors = ''){
+	// 何かエラーが渡されると、それを出力
+	if($errors){
+		print '以下のエラーを修正してください:<ul><li>';
+		print implode('</li><li>', $errors);
+		print '</li></ul>';
+	}
 ?>
 
 <form method="POST" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
 Your name: <input type="text" name="my_name">
 <br />
+<p>
+メールアドレス：<input type="text" name="email">
+</p>
+<p>年齢：<input type="text" name="age" size="2"></p>
+<p>
+デザートを選択してください:<select name="order">
+<?php
+foreach($GLOBALS['sweets'] as $choice){
+	print "<option>$choice</option>\n";
+}
+?>
+</select>
+</p>
 <input type="submit" value="Say Hello">
 <input type="hidden" name="_submit_check" value="1">
 </form>
@@ -19,12 +43,30 @@ Your name: <input type="text" name="my_name">
 }
 
 function validate_form(){
-	// my_nameの長さは少なくとも３文字あるか？
+	// エラーメッセージを格納する配列を初期化
+	$errors = array();
+
+	// 名前が短すぎる場合のチェック
 	if(mb_strlen($_POST['my_name']) < 3){
-		return false;
-	}else{
-		return true;
+		$errors[] = '名前は３文字以上で入力してください';
 	}
+
+	// メールアドエレスが入力されているかチェック
+	if(strlen($_POST['email']) == 0){
+		$errors[] = 'メールアドレスを入力してください';
+	}elseif(!preg_match('/^[^@\s]+@([-a-z0-9]+\.)+[a-z]{2,}$/i', $_POST['email'])){
+		$errors[] = '正しいメールアドレスを入力してください';
+	}
+
+	// 整数チェック
+	if($_POST['age'] != strval(intval($_POST['age']))){
+		$errors[] = '年齢は数値で入力してください';
+	}elseif($_POST['age'] <18 || $_POST['age'] > 65){
+		$errors[] = '年齢は１８歳以上６５歳以下で入力してください';
+	}
+
+	// エラーメッセージの配列（エラーがなければ空）を返す
+	return $errors;
 }
 ?>
 <!DOCTYPE html>
@@ -40,10 +82,10 @@ function validate_form(){
 // サブミットされたフォームパラメータをベースに
 // なすべきことをするロジック
 if(array_key_exists('_submit_check', $_POST)){ // サブミットされた?
-	if(validate_form()){ // $_POSTのチェック
-		process_form(); // 処理を実行
+	if($form_erros = validate_form()){ // $_POSTのチェック
+		show_form($form_erros); // フォームを再表示
 	}else{
-		show_form(); // フォームを再表示
+		process_form(); // 処理を実行
 	}
 }else{
 	show_form(); // フォームを表示
